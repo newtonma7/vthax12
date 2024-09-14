01d59d7d03ebe42c3b349bb49b118d76ce99d702
nguyerichard25 <nguye.richard25@gmail.com>
2024-09-14 10:45:43 -0400

    Further implemented chat box

--- pages/🙏Talk to Goku🙏.py
+++ pages/🙏Talk to Goku🙏.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import time
+
+def test():
+    response = "Hello Hello hello hello"
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
 
 #new assistant page
 st.set_page_config(
@@ -7,9 +14,28 @@ st.set_page_config(
 )
 
 
-<<<<<<< HEAD
 st.title("hello, this is goku")
 st.divider()
-=======
-st.title("hello")
->>>>>>> 0730e53fcc7969a7a0e9a09ff9a8c2a1c5487871
+st.markdown("Hello! Chat with me.")
+#initial message from Goku
+Goku = st.chat_message("Goku")
+Goku.write("👋 Hello! \nHow may I help you today")
+
+#init chat history list
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+#chat box
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("Start chatting")
+user = st.chat_message("user")
+if prompt:
+    user.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    #Goku response
+    with st.chat_message("Goku"):
+        response = st.write_stream(test())
+    st.session_state.messages.append({"role": "Goku", "content": response})
